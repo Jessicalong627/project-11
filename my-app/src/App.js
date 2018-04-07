d521d19435e1fff5c8416697ace4bc9555b8ff6f
Jessica Long <jessicalong627@gmail.com>
2018-04-06 20:26:10 -0400

    fixed nav bar

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -3,14 +3,10 @@ import {
   BrowserRouter,
   Route,
   Redirect,
-Switch
 } from 'react-router-dom';
 
 // App comp
  import Container from  './Container';
- import PathNotFound from './PathNotFound';
-
-
 
 class App extends Component {
 
@@ -24,19 +20,16 @@ class App extends Component {
 
   changeValue (text) { this.setState({query : text}); }
 
-//main app 
+//Main app
   render(){
     return (
     <BrowserRouter>
            <div className="container">
-         <Switch>
          <Route exact path='/' render={ () => <Redirect to={`/Nature`}/>}/>
          <Route  path='/search' render={ () => <Container changeValue={this.changeValue.bind(this)} value={this.state} />}/>
          <Route  path='/Nature' render={ () => <Container value='Nature'/>}/>
-        <Route  path='/NewYork' render={ () => <Container  value="NewYork"/>}/>
-        <Route  path='/Sunset' render={ () => <Container   value="Sunset" />}/>
-        <Route component={PathNotFound} />
-          </Switch>
+        <Route  path='/NewYork' render={ () => <Container value="NewYork"/>}/>
+        <Route  path='/Sunset' render={ () => <Container value="Sunset" />}/>
          </div>
   </BrowserRouter>
       );
